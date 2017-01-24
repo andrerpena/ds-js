f14939a9168f095e102923678db44b306956f5be
andrerpena <andrerpena@gmail.com>
2017-01-24 17:12:18 -0200

    Quicksort working and better tests for sorting

--- src/algo/sorter.js
+++ src/algo/sorter.js
@@ -1,3 +1,18 @@
+/**
+ * Swaps the i and j indexes of the array array 
+ * @param {any} array
+ * @param {any} i
+ * @param {any} j
+ */
+function swap(array, i, j) {
+    if (!array) throw Error('Argument \'array\' should be truthy');
+    if (i == j)
+        return;
+    var buffer = array[j];
+    array[j] = array[i];
+    array[i] = buffer;
+}
+
 module.exports = {
 
     /**
@@ -6,44 +21,82 @@ module.exports = {
      *      Average case:   O(N²)
      *      Best case:      O(N) -> When the array is already sorted
      * Space complexity:
-     *      Always:         O(N)
+     *      Always:         O(N) -> No extra allocation
      * @param {any} array
      */
-    bubbleSort: function(array) {
-        if(!array) throw Error('Argument \'array\' should be truthy');
-        for(var i = 0; i < array.length - 1; i++) {
+    bubbleSort: function (array) {
+        if (!array) throw Error('Argument \'array\' should be truthy');
+        for (var i = 0; i < array.length - 1; i++) {
             var swaps = 0;
-            for(var j = 0; j < array.length - 1; j++) {
-                if(array[j + 1] > array[j]) {
+            for (var j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
                     // swap
-                    var buffer = array[j + 1];
-                    array[j + 1] = array[j];
-                    array[j] = buffer;
+                    swap(array, j, j + 1);
                     swaps++;
                 }
             }
-            if(swaps === 0)
+            if (swaps === 0)
                 break;
         }
     },
 
-    quickSort: function(array) {
-        if(!array) throw Error('Argument \'array\' should be truthy');
+    quickSort: function (array) {
+        if (!array) throw Error('Argument \'array\' should be truthy');
+
+        /**
+         * This will assume the last item of the given array as the pivot.
+         * @param {any} array
+         * @returns the position of the pivot.
+         * Every item lower than the pivot will be to the left.
+         * Every item higher than the pivot will be to the right
+         */
+        function partition(a, lo, hi) {
+            if (!a) throw Error('Argument \'array\' should be truthy');
+            if (a.length == 1)
+                return 0;
+            var pivot = a[hi];
+            // i is the position of the first item that is greater than the pivot
+            // at the and. i will be swapped with the pivot and be returned
+            var i = lo;
+            for (var j = lo; j < hi; j++) {
+                if (a[j] <= pivot) {
+                    swap(a, i, j);
+                    i++;
+                }
+            }
+            swap(a, i, hi);
+            return i;
+        }
+
+        /**
+         * The actual quicksort algorithm 
+         * @param {any} array The array to sort
+         * @param {any} lo The starting index
+         * @param {any} hi The ending index
+         */
+        function qs(array, lo, hi) {
+            if (lo < hi) {
+                var i = partition(array, lo, hi);
+                qs(array, lo, i - 1);
+                qs(array, i + 1, hi);
+            }
+        }
+
+        qs(array, 0, array.length - 1);
+    },
+
+    mergeSort: function (array) {
+        if (!array) throw Error('Argument \'array\' should be truthy');
         return array;
     },
 
-    mergeSort: function(array) {
-        if(!array) throw Error('Argument \'array\' should be truthy');
+    heapSort: function (array) {
+        if (!array) throw Error('Argument \'array\' should be truthy');
         return array;
     },
 
-    heapSort: function(array) {
-        if(!array) throw Error('Argument \'array\' should be truthy');
-        return array;
-    },
-
-    insertionSort: function(array) {
-        if(!array) throw Error('Argument \'array\' should be truthy');
+    insertionSort: function (array) {
+        if (!array) throw Error('Argument \'array\' should be truthy');
         return array;
     }
 }
---
+++ test/sorterSpec.js
@@ -0,0 +1,126 @@
+var assert = require('chai').assert;
+var sorter = require('../src/algo/sorter');
+
+/**
+ * Returns an array of random values (with repetition) of the given length 
+ * @param {any} length
+ * @param {any} max
+ * @returns
+ */
+function getRandomArray(length, max) {
+    if (!length) throw Error('Argument \'length\' should be truthy');
+    if (!max) throw Error('Argument \'max\' should be truthy');
+    return Array.apply(null, Array(length)).map(function (_, i) {
+        return Math.round(Math.random() * max);
+    });
+}
+
+/**
+ * Generates a sorted array with the given length, starting at 0 
+ * @param {any} length
+ * @returns
+ */
+function generateSortedArray(length, asc) {
+    var result = [];
+    if(asc || asc === undefined) for(var i = 0; i < length; i++) result.push(i);
+    else for(var i = length - 1; i >= 0; i--) result.push(i);
+    return result;
+}
+
+/**
+ * Returns whether the given array is sorted 
+ * @param {any} array
+ * @returns
+ */
+function isArraySorted(array) {
+    if (!array) throw Error('Argument \'array\' should be truthy');
+    for (var i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1])
+            return false;
+    }
+    return true;
+}
+
+
+/**
+ * Return whether the 2 arrays contains the same elements in the same order 
+ * @param {any} array1
+ * @param {any} array2
+ * @returns
+ */
+function areArraysTheSame(array1, array2) {
+    if (!array1) throw Error('Argument \'array1\' should be truthy');
+    if (!array2) throw Error('Argument \'array2\' should be truthy');
+    if (array1.length != array2.length)
+        return false;
+    for (var i = 0; i < array1.length; i++) {
+        if (array1[i] != array2[i])
+            return false;
+    }
+    return true;
+}
+
+/**
+ * Tests a random array using the given sortFunction and the given size 
+ * @param {any} sortFunction
+ * @param {any} size
+ */
+function testRandomArray(sortFunction, size) {
+    if (!sortFunction) throw Error('Argument \'sortFunction\' should be truthy');
+    if (!size) throw Error('Argument \'size\' should be truthy');
+
+    // creates an array with the given size. Elements will range from 0 to size and possibly repeated
+    var array = getRandomArray(size, size);
+    // clone the array so we can sort it. We have to clone in order to detect if the sorting algorithm is damaging the original
+    var arrayClone = array.concat();
+    sortFunction(arrayClone);
+    assert.isTrue(isArraySorted(arrayClone));
+    assert.isTrue(areArraysTheSame(array.sort(function (a, b) { return a - b }), arrayClone));
+}
+
+/**
+ * Tests the specified sortFunction against the specified array 
+ * @param {any} sortFunction
+ * @param {any} size
+ */
+function testArray(sortFunction, array) {
+    if (!sortFunction) throw Error('Argument \'sortFunction\' should be truthy');
+    if (!array) throw Error('Argument \'array\' should be truthy');
+
+    // clone the array so we can sort it. We have to clone in order to detect if the sorting algorithm is damaging the original
+    var arrayClone = array.concat();
+    sortFunction(arrayClone);
+    assert.isTrue(isArraySorted(arrayClone));
+    assert.isTrue(areArraysTheSame(array.sort(function (a, b) { return a - b }), arrayClone));
+}
+
+function runSortingTestSuite(sortFunction) {
+    if (!sortFunction) throw Error('Argument \'sortFunction\' should be truthy');
+    it('sorted 5 items', function () {
+        testArray(sortFunction, [1, 2, 3, 4, 5]);
+    });
+    it('sorted descending 5 items', function () {
+        testArray(sortFunction, [5, 4, 3, 2, 1]);
+    });
+    it('small 5 items (repeating numbers)', function () {
+        testArray(sortFunction, [1, 1, 2, 2, 4]);
+    });
+    it('sorted 10,000 items', function () {
+        testArray(sortFunction, generateSortedArray(10000));
+    });
+    it('sorted descending 10,000 items', function () {
+        testArray(sortFunction, generateSortedArray(10000, false));
+    });
+    it('random 10,000 items', function () {
+        testRandomArray(sortFunction, 10000);
+    });
+}
+
+describe('sorter', function () {
+    describe('bubblesort', function () {
+        runSortingTestSuite(sorter.bubbleSort);
+    });
+    describe('quicksort', function () {
+        runSortingTestSuite(sorter.quickSort);
+    });
+});;
